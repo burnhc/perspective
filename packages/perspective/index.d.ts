8075363a275f51ad4c773a90e718136298ea7145
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-12-17 12:10:41 -0500

    Fix jupyterlab

--- packages/perspective/index.d.ts
+++ packages/perspective/index.d.ts
@@ -133,7 +133,7 @@ declare module "@finos/perspective" {
         schema(): Promise<Schema>;
         size(): Promise<number>;
         update(data: TableData, options?: {port_id?: number}): void;
-        view(config?: ViewConfig): View; // TODO: make async
+        view(config?: ViewConfig): Promise<View>;
         make_port(): number;
     };
 
