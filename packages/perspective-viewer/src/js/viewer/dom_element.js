ef18267095861527056f984025b71e1280b0ae7a
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-12-17 11:34:37 -0500

    Fix filter test for computed

--- packages/perspective-viewer/src/js/viewer/dom_element.js
+++ packages/perspective-viewer/src/js/viewer/dom_element.js
@@ -98,17 +98,11 @@ export class DomElement extends PerspectiveElement {
                 // Get all unique values for the column by pivoting on it.
                 this._table
                     .view({row_pivots: [name], columns: []})
-                    .then(view => {
-                        // set as a property so we can delete it after the
-                        // autocomplete choices are set.
-                        this._filter_view = view;
-                        return view.to_json();
-                    })
-                    .then(json => row.choices(this._autocomplete_choices(json)))
-                    .finally(() => {
-                        // Clean up the View on the Emscripten heap.
-                        this._filter_view.delete();
-                        delete this._filter_view;
+                    .then(v => [v, v.to_json()])
+                    .then(result => {
+                        let view = result[0];
+                        let json = result[1];
+                        json.then(json => row.choices(this._autocomplete_choices(json))).then(() => view.delete());
                     });
             }
         }
